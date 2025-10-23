234ad291275bb1092fe1d98a202b5b7b701d8016
Hassan Hashmi <hashmihassan47@gmail.com>
2025-10-23 14:04:59 -0400

    CPU auto moves
    - CPU can make moves on it's turn

--- lib/providers/game_provider.dart
+++ lib/providers/game_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -101,6 +103,38 @@ class GameProvider extends ChangeNotifier {
     return removedCards == (totalCards - 1);
   }
 
+  /// Calculate the count of untouched cards in a given row
+  int cardCountInRow(int row){
+    int count = 0;
+    for(int j = 0; j < _gameState[row].length; ++j){
+      if(_gameState[row][j] == CardState.untouched){
+        ++count;
+      }
+    }
+    return count;
+  }
+
+  /// Removes the passed number of cards from the requested heap.
+  ///   - if not possible throws an Out of cards exception
+  void removeCards(int requestedRow, int numCards){
+    print('CPU is removing $numCards from ${requestedRow + 1}');
+    try {
+
+      if(cardCountInRow(requestedRow) < numCards){
+        throw Exception('Cannot remove $numCards cards from row $requestedRow');
+      }
+
+      // Remove numCards
+      for(int j = 0; j < numCards; ++j){
+        if(_gameState[requestedRow][j] == CardState.untouched){
+          _gameState[requestedRow][j] = CardState.removed;
+        }
+      }
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
   /// Executes the cpus turn
   /// Has a timed delay to simulate thinking
   /// - Checks to see if player has won at beginning of turn
@@ -110,12 +144,14 @@ class GameProvider extends ChangeNotifier {
     if(isOneCardLeft()){
       print("Player has won");
     }
-    await Future.delayed(const Duration(seconds: 5)); 
+    await Future.delayed(const Duration(seconds: 2)); 
 
     // Formula to win nim game here:
     // TODO: Check for edge case where all heaps are size one
 
     int nimSum = 0;
+    List<int> heapSums = [0,0,0,0]; // initialize with 4 heaps
+
     // STEP 1: Get Binary sum of all non zero heaps and XOR sum
     for(int i = 0; i < _gameState.length; i++){
       int currHeapSum = 0;
@@ -124,18 +160,47 @@ class GameProvider extends ChangeNotifier {
           currHeapSum++;
         }
       }
+      heapSums[i] += currHeapSum; // Track the sum of each hap
       nimSum ^= currHeapSum;
     }
 
+    print('nimSum is $nimSum');
 
-    // STEP 2: If the nimSum is zero...
-    
+    // STEP 2: If the nimSum is zero remove one random card from any heap since CPU is in losing position
+    // otherwise play a winning move
+    if(nimSum == 0){
+      Random random = Random();
+      int min = 0;
+      int max = _gameState.length - 1;
+
+      bool validMoveMade = false;
+      while(!validMoveMade){
+        int randomHeapIndex = random.nextInt(max - min) + min;
+        // Find one random heap with at least 1 card and remove from it
+        if(cardCountInRow(randomHeapIndex) >= 1){
+          removeCards(randomHeapIndex, 1);
+          validMoveMade = true;
+        }
+      }
+    } else{
+      // find a heap where the nimSum XOR currHeapSum < currHeapSum
+      // reduce that heap such that its currHeapSum is eqaul to nimSum XOR original currHeapSum
+
+      for(int i = 0; i < heapSums.length; ++i){
+        if((nimSum ^ heapSums[i]) < heapSums[i]){
+          // we can reduce this heap
+          removeCards(i, heapSums[i] - (nimSum ^ heapSums[i]));
+          break;
+        }
+      }
+    }
     changeTurn(); // Change turn back to player
-
+    if(isOneCardLeft()){
+      print("CPU WINS");
+    }
   }
 
   /// Carries out the following actions:
-  /// - Check to see if the game is  at player turn start
   /// - Ensure at least one card is selected i.e Selected row is set
   /// - Converted selected cards into removed cards
   /// - Reset Selected row
@@ -144,11 +209,7 @@ class GameProvider extends ChangeNotifier {
   /// - Call CPU turn function //TODO
   ///
   void endPlayerTurn() {
-    //TODO: Checking win conditions should be at the start of player turn not when endPlayer turn is called
-    if(isOneCardLeft()){
-      //TODO: Player has won logic here
-      print('Game over. CPU won');
-    }
+
     if(_selectedRow == null){
       //TODO: Error message here
       return;
