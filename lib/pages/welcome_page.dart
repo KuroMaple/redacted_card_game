1bf93a43eea1dc9db538e09e2cd0043280be358a
Hassan Hashmi <hashmihassan47@gmail.com>
2025-10-25 12:05:43 -0400

    Responsiveness WelcomScreen
    - Handle different screen sizes for WelcomePage

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -8,33 +8,83 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: SingleChildScrollView(
+    // Responsive work
+    final currOrientation = MediaQuery.of(context).orientation;
+    final currWinWidth = MediaQuery.of(context).size.width;
+
+    print('Current width is $currWinWidth');
+    // 850 px
+
+    if (currOrientation == Orientation.landscape || currWinWidth > 850) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: FittedBox(
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
+            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Lottie.asset('assets/lotties/ace_of_spade.json'),
                 FittedBox(
-                  child: Text('REDACTED', style: KTextStyle.welcomeTitleText),
+                  child: Column(
+                    children: [
+                      Text('REDACTED', style: KTextStyle.welcomeTitleText),
+                      Lottie.asset('assets/lotties/ace_of_spade.json'),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 40.0),
                 FilledButton(
                   style: ButtonStyle(
                     minimumSize: WidgetStateProperty.all(Size(200, 75)),
                   ),
                   onPressed: () {
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
-                      return WidgetTree();
-                    },));
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return WidgetTree();
+                        },
+                      ),
+                    );
                   },
                   child: Text("Play", style: TextStyle(fontSize: 30)),
                 ),
               ],
             ),
           ),
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/lotties/ace_of_spade.json'),
+            FittedBox(
+              child: Text('REDACTED', style: KTextStyle.welcomeTitleText),
+            ),
+            SizedBox(height: 40.0),
+            FilledButton(
+              style: ButtonStyle(
+                minimumSize: WidgetStateProperty.all(Size(200, 75)),
+              ),
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              child: Text("Play", style: TextStyle(fontSize: 30)),
+            ),
+          ],
         ),
       ),
     );
